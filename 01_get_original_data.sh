73a84e1b66c37bc055ded4b2ff5f9e496656bef3
Sameer Jain <usa.jain@gmail.com>
2019-05-12 18:28:54 -0400

    Downloads wikipedia data and unzips it in data directory

--- 01_get_original_data.sh
+++ 01_get_original_data.sh
@@ -2,3 +2,4 @@
 
 wget -P ./data/ http://snap.stanford.edu/data/soc-sign-Slashdot090221.txt.gz  
 wget -P ./data/ http://snap.stanford.edu/data/soc-sign-epinions.txt.gz 
+wget -P ./data/ http://snap.stanford.edu/data/wikiElec.ElecBs3.txt.gz
--- 02_clean_original_data.sh
+++ 02_clean_original_data.sh
@@ -2,6 +2,7 @@
 
 gunzip -k ./data/soc-sign-Slashdot090221.txt.gz
 gunzip -k ./data/soc-sign-epinions.txt.gz
+gunzip -k ./data/wikiElec.ElecBs3.txt.gz
 
 sed 1,4d ./data/soc-sign-epinions.txt > ./data/epinions.txt 
 sed 1,4d ./data/soc-sign-Slashdot090221.txt > ./data/slashdot.txt
