cbd6aa865d27927e104389792502689dab03899b
Sameer Jain <usa.jain@gmail.com>
2019-05-12 19:27:19 -0400

    Modified scripts, replaced wget with curl

--- 01_get_original_data.sh
+++ 01_get_original_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-wget -P ./data/ http://snap.stanford.edu/data/soc-sign-Slashdot090221.txt.gz  
-wget -P ./data/ http://snap.stanford.edu/data/soc-sign-epinions.txt.gz 
-wget -P ./data/ http://snap.stanford.edu/data/wikiElec.ElecBs3.txt.gz
+curl http://snap.stanford.edu/data/soc-sign-Slashdot090221.txt.gz --output ./data/soc-sign-Slashdot090221.txt.gz 
+
+curl http://snap.stanford.edu/data/soc-sign-epinions.txt.gz --output ./data/soc-sign-epinions.txt.gz 
+
+curl http://snap.stanford.edu/data/wikiElec.ElecBs3.txt.gz --output ./data/wikiElec.ElecBs3.txt.gz
--- 02_clean_original_data.sh
+++ 02_clean_original_data.sh
@@ -10,3 +10,4 @@ sed 1,4d ./data/soc-sign-epinions.txt > ./data/epinions.txt
 sed 1,4d ./data/soc-sign-Slashdot090221.txt > ./data/slashdot.txt
 
 rm ./data/soc*
+rm ./data/wikiElec.ElecBs3.txt.gz
