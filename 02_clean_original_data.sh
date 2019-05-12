09fa798a0b25a57f5e07f402ca6206473232f59d
Sameer Jain <usa.jain@gmail.com>
2019-05-12 18:57:17 -0400

    Renamed wikipedia data to wiki.txt

--- 02_clean_original_data.sh
+++ 02_clean_original_data.sh
@@ -4,6 +4,8 @@ gunzip -k ./data/soc-sign-Slashdot090221.txt.gz
 gunzip -k ./data/soc-sign-epinions.txt.gz
 gunzip -k ./data/wikiElec.ElecBs3.txt.gz
 
+mv ./data/wikiElec.ElecBs3.txt ./data/wiki.txt
+
 sed 1,4d ./data/soc-sign-epinions.txt > ./data/epinions.txt 
 sed 1,4d ./data/soc-sign-Slashdot090221.txt > ./data/slashdot.txt
 
