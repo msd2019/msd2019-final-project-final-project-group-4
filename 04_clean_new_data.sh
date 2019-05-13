387eaade677a592cb5aa67adbaa937e4b0aff6dc
Sameer Jain <usa.jain@gmail.com>
2019-05-13 19:25:49 -0400

    Modified clean new data script to cut the time column from soc-sign-bitcoinotc.csv

--- 04_clean_new_data.sh
+++ 04_clean_new_data.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# decompress the gz file
 gunzip ./data/soc-sign-bitcoinotc.csv.gz
+
+# remove time column
+# only columns remaining are SNAP, TARGET, RATING
+cut -d, -f1-3 ./data/soc-sign-bitcoinotc.csv > ./data/bitcoinotc.csv
+
+rm ./data/soc-sign-bitcoinotc.csv
+mv ./data/bitcoinotc.csv ./data/soc-sign-bitcoinotc.csv 
