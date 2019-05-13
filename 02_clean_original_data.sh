ebb58e6a5f38ffe39523f6b88ad5956c511055b3
Sameer Jain <usa.jain@gmail.com>
2019-05-12 20:02:40 -0400

    Added names and basic structure of final report. Updated shell scripts to remove unnecessary lines from wiki data including beginning lines and non UTF-8 characters

--- 02_clean_original_data.sh
+++ 02_clean_original_data.sh
@@ -4,10 +4,13 @@ gunzip -k ./data/soc-sign-Slashdot090221.txt.gz
 gunzip -k ./data/soc-sign-epinions.txt.gz
 gunzip -k ./data/wikiElec.ElecBs3.txt.gz
 
-mv ./data/wikiElec.ElecBs3.txt ./data/wiki.txt
+mv ./data/wikiElec.ElecBs3.txt ./data/wikiElec.ElecBs3.txt
 
 sed 1,4d ./data/soc-sign-epinions.txt > ./data/epinions.txt 
 sed 1,4d ./data/soc-sign-Slashdot090221.txt > ./data/slashdot.txt
+sed 1,6d ./data/wikiElec.ElecBs3.txt > ./data/wiki.txt
+
+iconv -c -t UTF-8 <./data/wiki.txt> ./data/wiki-utf.txt
 
 rm ./data/soc*
-rm ./data/wikiElec.ElecBs3.txt.gz
+rm ./data/wikiElec.ElecBs3.*
